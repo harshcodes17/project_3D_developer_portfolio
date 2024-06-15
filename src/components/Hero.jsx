04ac6a22577be5ca43005a4cabf58ffe252ee8b3
harshcodes17 <harshabamane1718@gmail.com>
2024-06-15 16:18:17 +0530

    added personal info

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,10 +16,10 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Harshavardhan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
+            I am a tech enthusiast and a competetive Programmer  <br className='sm:block hidden' />
             interfaces and web applications
           </p>
         </div>
